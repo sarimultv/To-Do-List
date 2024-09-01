97a9b63316555938b5ef3b4bf05383d194aa9842
Sarimul Hoque Laskar <49907699+sarimultv@users.noreply.github.com>
2024-09-01 17:18:00 +0530

    Delete calculator directory
    Other directory

--- calculator/src/components/DisplayOutput.jsx
+++
@@ -1,9 +0,0 @@
-const DisplayOutput = ({ styles, displayVal }) => {
-  return (
-    <div className={styles.display}>
-      <input type="text" className="form-control" value={displayVal} readOnly />
-    </div>
-  );
-};
-
-export default DisplayOutput;
